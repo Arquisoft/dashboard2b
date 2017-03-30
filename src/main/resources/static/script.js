00788ac8f505df1fdd2fe2046016acb4b3a4d592
Fernando Contreras <fercreek@gmail.com>
2017-03-30 13:44:09 +0200

    Conexion usando EventSource

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,4 +1,15 @@
 angular.module('dashboard', [])
   .controller('dash', function($scope) {
     $scope.greeting = {id: 'xxx', content: 'Hello World!'}
-});
+
+        if (typeof(EventSource) !== "undefined") {
+            // Yes! Server-sent events support!
+            var source = new EventSource('/streams');
+
+            source.onmessage = function (event) {
+                console.log("Im here bitch");
+                console.log(event);
+            };
+        }
+    }
+);

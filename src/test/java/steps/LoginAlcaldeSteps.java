dee76e9305a4b3d53acfeb0328d7292ab59ff3dd
uo245226 <uo245226@uniovi.es>
2017-04-04 15:15:57 +0200

    Añadida prueba cucumber y selenium

--- src/test/java/steps/LoginAlcaldeSteps.java
+++ src/test/java/steps/LoginAlcaldeSteps.java
@@ -1,17 +1,45 @@
 package steps;
 
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import utils.SeleniumUtils;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import static org.junit.Assert.*;
+
 
 public class LoginAlcaldeSteps {
 
+	WebDriver driver; 
+	
+	@Before
+	public void run()
+	{
+		driver = new HtmlUnitDriver();
+		driver.get("http://localhost:8090");		
+	}
+	
+	@After
+	public void end()
+	{
+		//Cerramos el navegador
+		driver.quit();
+	}
+	
 	@When("^un usuario de tipo alcalde se loguea con usuario \"(.+)\" y password \"(.+)\"$")
 	public void i_login_with_name_and_password(String name, String password) throws Throwable {
-		// Implementar
+		SeleniumUtils.textoPresentePagina(driver, "Dashboard2b");
 	}
 
 	@Then("^el usuario recibe la pantalla inicial$")
 	public void i_receive_a_welcome_message() throws Throwable {
-		// Implementar
+		assertTrue(true);
 	}
 }
--- src/test/java/steps/LoginConcejalSteps.java
+++ src/test/java/steps/LoginConcejalSteps.java
@@ -1,5 +1,6 @@
 package steps;
 
+import static org.junit.Assert.assertTrue;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
@@ -7,12 +8,12 @@ public class LoginConcejalSteps {
 
 	@When("^un usuario de tipo concejal se loguea con usuario \"(.+)\" y password \"(.+)\"$")
 	public void i_login_with_name_and_password(String name, String password) throws Throwable {
-		// Implementar
+		assertTrue(true);
 	}
 
 	@Then("^el usuario recibe la pantalla inicial$")
 	public void i_receive_a_welcome_message() throws Throwable {
-		// Implementar
+		assertTrue(true);
 	}
 
 }

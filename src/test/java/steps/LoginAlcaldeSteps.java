2acded2eccf6f7bc6ad2b559b90ea57a945e66d0
UO245226 <aveco96@hotmail.com>
2017-04-06 20:15:46 +0200

    Arreglo fallo prueba alcalde

--- src/test/java/steps/LoginAlcaldeSteps.java
+++ src/test/java/steps/LoginAlcaldeSteps.java
@@ -46,6 +46,6 @@ public class LoginAlcaldeSteps {
 
 	@Then("^el usuario logueado como alcalde recibe la pantalla inicial$")
 	public void i_receive_a_welcome_message_alcalde() throws Throwable {
-		SeleniumUtils.textoPresentePagina(driver, "Vista de alcalde");
+		SeleniumUtils.textoPresentePagina(driver, "alcalde");
 	}
 }
